6eb27e6c294f023ee646c3bba10634338323b577
mohitkachhadiya <mohitkachhadiya312@gmail.com>
2020-05-11 11:31:47 +0530

    dashboard issue fixed

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -198,18 +198,6 @@ export class DashboardPage implements OnInit {
 		});
 	}
 
-	hmsToSeconds(s) {
-		var b = s.split(':');
-		return b[0]*3600 + b[1]*60 + (+b[2] || 0);
-	}
-
-	secondsToHMS(secs) {
-		function z(n){return (n<10?'0':'') + n;}
-		var sign = secs < 0? '-':'';
-		secs = Math.abs(secs);
-		return sign + z(secs/3600 |0) + ':' + z((secs%3600) / 60 |0) + ':' + z(secs%60);
-	}
-
 	async opensnack() {
 		const toast = await this._toast.create({
 			message: 'Fill Attendence Successfully.',
